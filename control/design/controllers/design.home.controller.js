0b6033d2c6c2c916d422b45ef567d10f19cfa31d
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2015-09-21 15:53:34 +0530

    Added angular structure in design section. View controller

---
+++ control/design/controllers/design.home.controller.js
@@ -0,0 +1,10 @@
+'use strict';
+
+(function (angular, window) {
+  angular
+    .module('eventsManualPluginDesign')
+    .controller('DesignHomeCtrl', ['$scope',
+      function ($scope) {
+
+      }]);
+})(window.angular);
