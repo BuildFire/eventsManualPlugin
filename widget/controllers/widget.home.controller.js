6a124d43c7422e8ed80526174ebc0c9ceae5d569
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2015-09-21 14:45:02 +0530

    Updated widget section with angular app structure. Showed event list view in ng-view.

---
+++ widget/controllers/widget.home.controller.js
@@ -0,0 +1,9 @@
+'use strict';
+
+(function (angular) {
+  angular.module('eventsManualPluginWidget')
+    .controller('WidgetHomeCtrl', ['$scope',
+      function ($scope) {
+
+      }])
+})(window.angular);
